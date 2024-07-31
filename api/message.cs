83fc9a1de0bae68c0e2c74161b38ddebef5d8f03
Matthew Cavallo <macavall@microsoft.com>
2024-07-31 12:01:15 -0500

    API Added with Function App

---
+++ api/message.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace macavall
+{
+    public static class message
+    {
+        [FunctionName("message")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string responseMessage = "Hello from API!!!";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
